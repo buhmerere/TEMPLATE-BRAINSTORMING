c5c5949135e6035e0873da000d7c7628a703d4a1
buhmerere <165039314+buhmerere@users.noreply.github.com>
2024-05-23 12:01:10 -0300

    .

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,4 +1,4 @@
-const nomes = ["Fernanda", "Giuliana", "Maria Eduarda", "Marcelo", "Amanda", "Gustavo", "Gabriel"];
+const nomes = ["Julia", "Dayana", "Maria Cecilia", "Mathew", "Ana Clara", "Guilhermo", "Gabriela"];
 
 export function aleatorio (lista){
     const posicao = Math.floor(Math.random()* lista.length);
